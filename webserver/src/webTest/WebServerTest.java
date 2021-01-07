3b759faa374c5421243c086ee6815eb12ec76fbb
andradapovian <48749153+andradapovian@users.noreply.github.com>
2021-01-07 12:59:29 +0200

    added web testing

---
+++ webserver/src/webTest/WebServerTest.java
@@ -0,0 +1,48 @@
+package webTest;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class WebServerTest {
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:/Users/andra/Downloads/geckodriver-v0.28.0-win64/geckodriver.exe"); 
+		WebDriver driver = new FirefoxDriver();
+//		driver.navigate().to("http://seleniumsimplified.com");
+//		Assert.assertTrue("title should start differently", driver.getTitle().startsWith("Selenium Simplified"));
+//		driver.close();
+		driver.get("http://localhost:8080/");
+//		WebElement element = driver.findElement(By.name("q"));
+//
+//        // Enter something to search for
+//        element.sendKeys("Cheese!");
+//
+//        // Now submit the form. WebDriver will find the form for us from the element
+//        element.submit();
+//
+//        // Check the title of the page
+//        System.out.println("Page title is: " + driver.getTitle());
+//        
+//        // Google's search is rendered dynamically with JavaScript.
+//        // Wait for the page to load, timeout after 10 seconds
+//        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+//            public Boolean apply(WebDriver d) {
+//                return d.getTitle().toLowerCase().startsWith("cheese!");
+//            }
+//        });
+//
+//        // Should see: "cheese! - Google Search"
+//        System.out.println("Page title is: " + driver.getTitle());
+//        
+        //Close the browser
+//        driver.quit();
+		
+	}
+}
